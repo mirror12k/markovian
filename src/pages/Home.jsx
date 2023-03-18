b8b9664f3025cf6cf3e8ec2937bf78b407dac3b0
mirror <mirror12k@gmail.com>
2023-03-18 18:16:56 -0400

    - pretty and neat

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,36 +8,52 @@ import { random_pages, get_munged_page_text, make_markov_chain, generate_sentenc
 const Home = () => {
 	const [loading, setloading] = useState(false);
 	const [content, setcontent] = useState(undefined);
+	const chainref = useRef({});
 
 	const loadcontent = async () => {
 		setloading(true);
-		var pages = await random_pages();
+		// var pages = await random_pages();
 		var chain = {};
-		make_markov_chain(await get_munged_page_text(pages[0]), chain);
-		make_markov_chain(await get_munged_page_text(pages[1]), chain);
-		make_markov_chain(await get_munged_page_text(pages[2]), chain);
-		make_markov_chain(await get_munged_page_text(pages[3]), chain);
-		make_markov_chain(await get_munged_page_text(pages[4]), chain);
+		// make_markov_chain(await get_munged_page_text('Division Street School'), chain);
+		for (var i = 0; i < 1;) {
+			var pages = await random_pages();
+			if (pages.length > 0)
+			for (var p = 0; p < pages.length; p++) {
+				console.log("loaded page...");
+				make_markov_chain(await get_munged_page_text(pages[p]), chainref.current);
+				i++;
+			}
+		}
+		// make_markov_chain(await get_munged_page_text(pages[1]), chain);
+		// make_markov_chain(await get_munged_page_text(pages[2]), chain);
+		// make_markov_chain(await get_munged_page_text(pages[3]), chain);
+		// make_markov_chain(await get_munged_page_text(pages[4]), chain);
+		console.log(chainref.current);
 
 		setloading(false);
-		setcontent([1,2,3,].map(i => generate_sentence(chain)).join(' '));
+		var text = [1,].map(i => generate_sentence(chainref.current)).join(' ');
+		setcontent(text.length > 1500 ? text.substring(0, 1500) + '...' : text);
 	};
+
+	const content_words = content?.split(' ');
 
 	return (loading ? <div><h1>Loading...</h1></div> :
 		<div>
 			{/*<ThemeSwitch />*/}
 			{content
-				? <div>
-					<h2 className="fade-in-text">{content.split(' ').map((t, i) => <span key={t + i} style={{animationDelay: '' +(i / 10)+'s'}}>{t} </span>)}</h2>
+				? <div className="m-4 p-4">
+					<h2 className="fade-in-text text-justify">{content_words.map((t, i) => <span key={t + i} style={{animationDelay: '' +(i / 10)+'s'}}>{t} </span>)}</h2>
 					<div className="text-center">
-						<button className="btn btn-primary" onClick={loadcontent}><h1>Generate again!</h1></button>
+						<button className="btn btn-primary fade-in-button" onClick={loadcontent} style={{animationDelay: '' +(content_words.length / 10)+'s'}}>
+							<h1>Dream again?</h1>
+						</button>
 					</div>
 				</div>
 
 
 
 				: <div className="text-center">
-					<button className="btn btn-primary" onClick={loadcontent}><h1>Click me!</h1></button>
+					<button className="btn btn-primary" onClick={loadcontent}><h1>Dream?</h1></button>
 				</div>}
 		</div>
 	);
--- src/util/markov.jsx
+++ src/util/markov.jsx
@@ -4,23 +4,35 @@ export const random_pages = () => fetch('https://en.wikipedia.org/w/api.php?acti
 export const query_page = page => fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=' + page + '&formatversion=2&rvprop=content&rvslots=*&origin=*').then(r => r.json()).then(j => j.query.pages[0].revisions[0].slots.main.content)
 export const strip_tags = s => s
 		.replace(/\n/g, ' ')
-		.replace(/\{\{[^{}]*\}\}|<[^<>]*>/g, "")
+		.replace(/==\s*(References|External links|See also)\s*==[^\x00]*/g, "")
+		.replace(/==\s*([a-zA-Z0-9\- :]+)\s*==/g, "")
+		.replace(/<(\w+)>[^<>]*<\/\1>/g, "")
+		.replace(/<gallery>[^\x00]*?<\/gallery>/g, "")
+		.replace(/\{\{([^{}]|\{\{[^{}]*\}\})*\}\}|<[^<>]*>/g, "")
+		.replace(/\{\{([^{}]|\{\{[^{}]*\}\})*\}\}|<[^<>]*>/g, "")
 		.replace(/'''|''|==/g, "")
-		.replace(/\[\[File([^\[\]]|\[\[[^\[\]]*\]\])*\]\]/g, '')
+		.replace(/\[\[(File|Image)([^\[\]]|\[\[[^\[\]]*\]\])*\]\]/g, '')
+		.replace(/\[\[(File|Image)([^\[\]]|\[\[[^\[\]]*\]\])*\]\]/g, '')
 		.replace(/\[\[([^\[\]|]*)\|[^\[\]|]*\]\]/g, "$1")
 		.replace(/\[\[([^\[\]]*)\]\]/g, "$1")
 		.replace(/\[([^\[\]]*)\]/g, "")
 		.replace(/\([^\(\)]*\)/g, "")
 
 export const munge_text = s => s
-		.replace(/[^a-zA-Z0-9,. \-]/g, '')
+		.replace(/[^a-zA-Z0-9,.:\/ \-]/g, '')
 		.replace(/(\w{2,})([.,])/g, "$1 $2 ")
 		.replace(/\s\s+/g, " ")
 		.replace(/([.,] )\1+/g, "$1")
-		.replace(/\s+,/g, ",").split(" . ").map(s => s + '.')
+		.replace(/\s+,/g, ",")
+		.split(" . ")
+		.filter(s => s.length > 5)
+		.map(s => s + '.')
+		.join(' ')
 export const get_munged_page_text = page => query_page(page).then(s => munge_text(strip_tags(s)))
 
-export const load_markov_chain = (t, v) => v.split(' ').filter(s => s !== '').reduce((p, s) => { p in t ? t[p].push(s) : t[p] = [ s ]; return s }, '')
-export const make_markov_chain = (l, t) => { t = t || {}; l.forEach(s => load_markov_chain(t, s)); return t }
+export const glue_context_words = w => { var g = []; var chain = ''; w.forEach(s => { if (s.match(/^(an?|as|at|by|but|the|which|from|that|of|would|could|can|was|is|on|in|for|are|and|to|has|to)$/i)) chain += s + ' '; else { g.push(chain + s); chain = ''; } }); return g }
+export const load_markov_chain = (t, v) => glue_context_words(v.split(' ').filter(s => s !== '')).reduce((p, s) => { p in t ? t[p].push(s) : t[p] = [ s ]; return s }, '')
+export const make_markov_chain = (l, t) => { t = t || {}; load_markov_chain(t, l); console.log(l, t); return t }
+// export const make_markov_chain = (l, t) => { t = t || {}; l.forEach(s => load_markov_chain(t, s)); return t }
 export const loop_while = (v, f) => { var l = [v]; while (v = f(v)) l.push(v); return l }
-export const generate_sentence = t => loop_while(t[''].random(), v => { return t[v]?.random() } ).join(' ')
+export const generate_sentence = t => loop_while(t[''].random(), v => { return t[v]?.random() } ).join(' ').replace(/&nbsp;/g, ' ')
